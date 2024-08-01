b9953f1af52a9c5ae6cc64f75b703214ae86a9b7
Willy Saronamihardja <wsaronam@gmail.com>
2024-08-01 01:21:22 -0700

    Small updates
    - number generation for dice (no more manual/hard coding)
    - button to reroll dice values

--- components/App.tsx
+++ components/App.tsx
@@ -2,20 +2,42 @@ import React from "react"
 import Die from "./Die.tsx"
 
 export default function App() {
+
+  const [currentDice, setCurrentDice]: [currentDice: number[], setCurrentDice: any] = React.useState(randomizeAllDice());
+
+
+
+
+  function randomizeAllDice(): number[] {
+    /*
+    Randomizes numOfDice amount of dice values (1-6), puts them into an array, and returns that array.
+    */
+    const diceArr: number[] = [];
+    const numOfDice: number = 10;
+    for (let i = 0; i < numOfDice; i++) {
+      const currentDieValue: number = Math.ceil(Math.random() * 6); // Math.random() * (max - min) + min;
+      diceArr.push(currentDieValue);
+    }
+    return diceArr;
+  }
+
+  function rerollButton(): void {
+    /*
+    Used by the reroll button to reroll all the dice.  This will eventually not reroll user-selected dice.
+    */
+    setCurrentDice(randomizeAllDice());
+  }
+
+
+
+
   return (
     <main>
       <div className="dice-container">
-        <Die value="1"/>
-        <Die value="2"/>
-        <Die value="3"/>
-        <Die value="4"/>
-        <Die value="5"/>
-        <Die value="6"/>
-        <Die value="7"/>
-        <Die value="8"/>
-        <Die value="9"/>
-        <Die value="0"/>
+        {currentDice.map(die => <Die value={die} />)}
       </div>
+
+      <button id="newDiceButton" onClick={rerollButton}>Roll new dice</button>
       
     </main>
   );
